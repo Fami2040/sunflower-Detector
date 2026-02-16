accd139344f2829dc933cd1c176a0b5102d205f0
Fami Abrahim <107669730+Fami2040@users.noreply.github.com>
2026-02-17 00:25:29 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -69,7 +69,7 @@ SLICE_SIZE = int(os.getenv("SLICE_SIZE", "800"))       # 800 = best balance for 
 OVERLAP = float(os.getenv("OVERLAP", "0.15"))          # default overlap
 
 # ---- Thresholds (LOW to reduce FN) ----
-CONF_THR = float(os.getenv("CONF_THR", "0.2"))
+CONF_THR = float(os.getenv("CONF_THR", "0.15"))
 NMS_IOU = float(os.getenv("NMS_IOU", "0.4"))
 
 # ---- Telegram / performance ----
